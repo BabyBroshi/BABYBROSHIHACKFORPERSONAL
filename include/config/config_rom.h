eed2d630cc76f74e5019b7ca3d2d64dba678733c
gooferguy <158222100+BabyYoshiOfficial@users.noreply.github.com>
2025-10-11 10:42:05 -0400

    beta

--- include/config/config_rom.h
+++ include/config/config_rom.h
@@ -53,4 +53,4 @@
  * RCVI hack. Increases performance on emulator, and does nothing on console.
  * Might break on some emulators. Use at your own risk, and don't use it unless you actually need the extra performance.
  */
-#define RCVI_HACK
+// #define RCVI_HACK

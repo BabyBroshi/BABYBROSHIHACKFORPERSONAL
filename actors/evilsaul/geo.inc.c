261c76e2416ead93147ef60be5670cc5d04e5f11
gooferguy <158222100+BabyYoshiOfficial@users.noreply.github.com>
2025-10-11 10:56:11 -0400

    a

--- actors/evilsaul/geo.inc.c
+++ actors/evilsaul/geo.inc.c
@@ -3,7 +3,8 @@
 const GeoLayout evilsaul_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
-		GEO_BILLBOARD_WITH_PARAMS_AND_DL(LAYER_ALPHA, 0, 0, 0, evilsaul_evilsaul_mesh_layer_4),
+		GEO_DISPLAY_LIST(LAYER_OPAQUE, evilsaul_SaulModel_mesh_layer_1),
+		GEO_DISPLAY_LIST(LAYER_ALPHA, evilsaul_SaulModel_mesh_layer_4),
 	GEO_CLOSE_NODE(),
 	GEO_END(),
 };

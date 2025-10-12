017e5bb3f73b7917b812de61bfc8a1c3df8b2309
gooferguy <158222100+BabyYoshiOfficial@users.noreply.github.com>
2025-10-11 21:58:57 -0400

    twistupturntableact3parkourstuff

--- levels/ccm/model.inc.c
+++ levels/ccm/model.inc.c
@@ -1,3 +1,52 @@
+Vtx ccm_dl_act3parkourstar_mesh_layer_1_vtx_cull[8] = {
+	{{ {-181, -181, 181}, 0, {0, 0}, {0, 0, 0, 0} }},
+	{{ {-181, 181, 181}, 0, {0, 0}, {0, 0, 0, 0} }},
+	{{ {-181, 181, -181}, 0, {0, 0}, {0, 0, 0, 0} }},
+	{{ {-181, -181, -181}, 0, {0, 0}, {0, 0, 0, 0} }},
+	{{ {181, -181, 181}, 0, {0, 0}, {0, 0, 0, 0} }},
+	{{ {181, 181, 181}, 0, {0, 0}, {0, 0, 0, 0} }},
+	{{ {181, 181, -181}, 0, {0, 0}, {0, 0, 0, 0} }},
+	{{ {181, -181, -181}, 0, {0, 0}, {0, 0, 0, 0} }},
+};
+
+Vtx ccm_dl_act3parkourstar_mesh_layer_1_vtx_0[24] = {
+	{{ {-181, -181, 181}, 0, {368, 1008}, {127, 0, 0, 255} }},
+	{{ {-181, -181, -181}, 0, {368, 752}, {127, 0, 0, 255} }},
+	{{ {-181, 181, -181}, 0, {624, 752}, {127, 0, 0, 255} }},
+	{{ {-181, 181, 181}, 0, {624, 1008}, {127, 0, 0, 255} }},
+	{{ {-181, -181, -181}, 0, {368, 752}, {0, 0, 127, 255} }},
+	{{ {181, -181, -181}, 0, {368, 496}, {0, 0, 127, 255} }},
+	{{ {181, 181, -181}, 0, {624, 496}, {0, 0, 127, 255} }},
+	{{ {-181, 181, -181}, 0, {624, 752}, {0, 0, 127, 255} }},
+	{{ {181, -181, -181}, 0, {368, 496}, {129, 0, 0, 255} }},
+	{{ {181, -181, 181}, 0, {368, 240}, {129, 0, 0, 255} }},
+	{{ {181, 181, 181}, 0, {624, 240}, {129, 0, 0, 255} }},
+	{{ {181, 181, -181}, 0, {624, 496}, {129, 0, 0, 255} }},
+	{{ {181, -181, 181}, 0, {368, 240}, {0, 0, 129, 255} }},
+	{{ {-181, -181, 181}, 0, {368, -16}, {0, 0, 129, 255} }},
+	{{ {-181, 181, 181}, 0, {624, -16}, {0, 0, 129, 255} }},
+	{{ {181, 181, 181}, 0, {624, 240}, {0, 0, 129, 255} }},
+	{{ {-181, -181, -181}, 0, {112, 496}, {0, 127, 0, 255} }},
+	{{ {-181, -181, 181}, 0, {112, 240}, {0, 127, 0, 255} }},
+	{{ {181, -181, 181}, 0, {368, 240}, {0, 127, 0, 255} }},
+	{{ {181, -181, -181}, 0, {368, 496}, {0, 127, 0, 255} }},
+	{{ {181, 181, -181}, 0, {624, 496}, {0, 129, 0, 255} }},
+	{{ {181, 181, 181}, 0, {624, 240}, {0, 129, 0, 255} }},
+	{{ {-181, 181, 181}, 0, {880, 240}, {0, 129, 0, 255} }},
+	{{ {-181, 181, -181}, 0, {880, 496}, {0, 129, 0, 255} }},
+};
+
+Gfx ccm_dl_act3parkourstar_mesh_layer_1_tri_0[] = {
+	gsSPVertex(ccm_dl_act3parkourstar_mesh_layer_1_vtx_0 + 0, 24, 0),
+	gsSP2Triangles(0, 1, 2, 0, 0, 2, 3, 0),
+	gsSP2Triangles(4, 5, 6, 0, 4, 6, 7, 0),
+	gsSP2Triangles(8, 9, 10, 0, 8, 10, 11, 0),
+	gsSP2Triangles(12, 13, 14, 0, 12, 14, 15, 0),
+	gsSP2Triangles(16, 17, 18, 0, 16, 18, 19, 0),
+	gsSP2Triangles(20, 21, 22, 0, 20, 22, 23, 0),
+	gsSPEndDisplayList(),
+};
+
 Vtx ccm_dl_level_mesh_mesh_layer_1_vtx_cull[8] = {
 	{{ {-2581, 0, 4115}, 0, {0, 0}, {0, 0, 0, 0} }},
 	{{ {-2581, 5581, 4115}, 0, {0, 0}, {0, 0, 0, 0} }},
@@ -384,6 +433,22 @@ Gfx ccm_dl_level_mesh_mesh_layer_1_tri_1[] = {
 	gsSPEndDisplayList(),
 };
 
+Gfx mat_ccm_dl_wowie[] = {
+	gsSPLightColor(LIGHT_1, 0xFFFFFFFF),
+	gsSPLightColor(LIGHT_2, 0x7F7F7FFF),
+	gsDPPipeSync(),
+	gsDPSetCombineLERP(0, 0, 0, SHADE, 0, 0, 0, ENVIRONMENT, 0, 0, 0, SHADE, 0, 0, 0, ENVIRONMENT),
+	gsDPSetAlphaDither(G_AD_NOISE),
+	gsSPTexture(65535, 65535, 0, 0, 1),
+	gsSPEndDisplayList(),
+};
+
+Gfx mat_revert_ccm_dl_wowie[] = {
+	gsDPPipeSync(),
+	gsDPSetAlphaDither(G_AD_DISABLE),
+	gsSPEndDisplayList(),
+};
+
 Gfx mat_ccm_dl_walls[] = {
 	gsSPLightColor(LIGHT_1, 0x0812AEFF),
 	gsSPLightColor(LIGHT_2, 0x020455FF),
@@ -413,6 +478,17 @@ Gfx mat_ccm_dl_floor[] = {
 Gfx mat_revert_ccm_dl_floor[] = {
 	gsDPPipeSync(),
 	gsDPSetAlphaDither(G_AD_DISABLE),
+	gsSPEndDisplayList(),
+};
+
+Gfx ccm_dl_act3parkourstar_mesh_layer_1[] = {
+	gsSPClearGeometryMode(G_LIGHTING),
+	gsSPVertex(ccm_dl_act3parkourstar_mesh_layer_1_vtx_cull + 0, 8, 0),
+	gsSPSetGeometryMode(G_LIGHTING),
+	gsSPCullDisplayList(0, 7),
+	gsSPDisplayList(mat_ccm_dl_wowie),
+	gsSPDisplayList(ccm_dl_act3parkourstar_mesh_layer_1_tri_0),
+	gsSPDisplayList(mat_revert_ccm_dl_wowie),
 	gsSPEndDisplayList(),
 };
 
